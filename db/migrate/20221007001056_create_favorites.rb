0f299b6d6aa726b0bc4b07c70ea534d61d51d8f0
EC2 Default User <ec2-user@ip-172-31-38-10.ap-northeast-1.compute.internal>
2022-10-08 02:22:07 +0000

    fix migrate

--- db/migrate/20221007001056_create_favorites.rb
+++ db/migrate/20221007001056_create_favorites.rb
@@ -3,6 +3,7 @@ class CreateFavorites < ActiveRecord::Migration[6.1]
     create_table :favorites do |t|
       
       t.string :name
+      t.text :comment
       t.integer :image_id
       t.integer :amount
       t.detetime :deadline
