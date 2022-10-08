2fb10f9451f884e5ef9178b7bea105369024c6f7
EC2 Default User <ec2-user@ip-172-31-38-10.ap-northeast-1.compute.internal>
2022-10-08 12:09:08 +0000

    sign_up画面までできる

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  validates_uniqueness_of :name
+  validates_presence_of :name
+
+  def email_required?
+    false
+  end
+
+  def email_changed?
+    false
+  end
 end
