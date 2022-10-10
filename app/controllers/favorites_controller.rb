645f5d76d8508fe41ad34530c74a3f5ff81daad5
EC2 Default User <ec2-user@ip-172-31-38-10.ap-northeast-1.compute.internal>
2022-10-10 05:30:24 +0000

    favorite controller

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,35 @@
 class FavoritesController < ApplicationController
+  
+  def index
+    @favorites = Favorite.all
+  end
+  
+  def show
+    @favorite = Favorite.find(params[:id])
+  end
+  
+  def create
+    @favorite = Favorite.new(favorite_params)
+    @favorite.save
+    redirect_to favorites_path
+  end
+  
+  def destory
+    @favorite = Favorite.find(params[:id])
+    @favorite.destory
+    redirect_to favorites_path
+  end
+  
+  def update
+    @favorite = Favorite.find(params[:id])
+    @faorite.update
+    redirect_to favorites_path
+  end
+  
+  
+  private
+  
+  def favorite_params
+    params.require(:favorite).permit(:name, :storage_id, :comment, :image_id, :amount, :deadline, :owner, :purchase_status)
+  end
 end
