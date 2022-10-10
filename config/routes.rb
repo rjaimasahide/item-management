c5bae757b8c2ffe66bd4975d40dfa9f0605ee630
EC2 Default User <ec2-user@ip-172-31-38-10.ap-northeast-1.compute.internal>
2022-10-10 07:06:59 +0000

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   resources :storages, only: [:new, :show, :index, :create, :destroy]
 
-  resources :items, only: [:show, :new, :edit, :create, :destroy, :update]
+  resources :items, only: [:new, :edit, :create, :destroy, :update]
 
-  resources :favorites, only: [:index, :show, :new, :create, :destroy]
+  resources :favorites, only: [:index, :edit, :new, :create, :destroy, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
