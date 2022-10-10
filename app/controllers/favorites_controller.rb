eaac642296532a0515444cf79a0f735d104e05db
EC2 Default User <ec2-user@ip-172-31-38-10.ap-northeast-1.compute.internal>
2022-10-10 08:21:24 +0000

    favorite show画面 追加

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,5 +1,5 @@
 class FavoritesController < ApplicationController
-  
+
   def index
     @favorites = Favorite.all
   end
@@ -7,26 +7,36 @@ class FavoritesController < ApplicationController
   def show
     @favorite = Favorite.find(params[:id])
   end
-  
+
+  def edit
+    @favorite = Favorite.find(params[:id])
+  end
+
   def new
     @favorite = Favorite.new
   end
-  
+
   def create
     @favorite = Favorite.new(favorite_params)
     @favorite.save
     redirect_to favorites_path
   end
-  
+
   def destory
     @favorite = Favorite.find(params[:id])
     @favorite.destory
     redirect_to favorites_path
   end
-  
-  
+
+  def update
+    @favorite = Favorite.find(params[:id])
+    @favorite.update(favorite_params)
+    redirect_to favorite_path(@favorite)
+  end
+
+
   private
-  
+
   def favorite_params
     params.require(:favorite).permit(:name, :storage_id, :comment, :image_id, :amount, :deadline, :owner, :purchase_status)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
 
   resources :items, only: [:new, :edit, :create, :destroy, :update]
 
-  resources :favorites, only: [:index, :edit, :new, :create, :destroy, :update]
+  resources :favorites, only: [:index, :show, :edit, :new, :create, :destroy, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
