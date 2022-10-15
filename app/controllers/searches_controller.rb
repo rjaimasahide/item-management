0dd467132df8f9278c01b01f0a5b4255a995b39b
EC2 Default User <ec2-user@ip-172-31-38-10.ap-northeast-1.compute.internal>
2022-10-15 14:05:45 +0000

    検索機能　修正

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,6 +1,7 @@
 class SearchesController < ApplicationController
 
   def search
-    @item = Item.looks(params[:word],)
+    @item = Item.looks(params[:word])
+    @items = @item.where(user_id: current_user.id)
   end
 end
