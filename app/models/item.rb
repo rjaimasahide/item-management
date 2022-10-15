b672febbddf2333710687a14b4bcf14bc3c67437
EC2 Default User <ec2-user@ip-172-31-38-10.ap-northeast-1.compute.internal>
2022-10-15 05:43:31 +0000

    あと何日表示追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,9 +8,15 @@ class Item < ApplicationRecord
   validates :name, presence:true
   validates :storage_id, presence:true
   validates :amount, presence:true
-
+  
+  #検索機能
   def self.looks(word)
     @item = Item.where("name LIKE?","%#{word}%")
+  end
+  
+  #deadline(期限)までの日数
+  def period
+    (self.deadline - Date.current).to_i
   end
 
   def get_image_id
