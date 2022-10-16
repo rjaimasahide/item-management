6838991c4741f0eaeddb1b62db0d35e26594aad5
EC2 Default User <ec2-user@ip-172-31-38-10.ap-northeast-1.compute.internal>
2022-10-16 08:02:58 +0000

    コメント機能編集

--- app/controllers/item_comments_controller.rb
+++ app/controllers/item_comments_controller.rb
@@ -1,21 +1,21 @@
 class ItemCommentsController < ApplicationController
-  
+
   def create
     @item_comment = ItemComment.new(item_comment_params)
     @item_comment.save
-    redirect_to storages_path
+    redirect_to edit_item_path(@item_comment.item_id)
   end
-  
+
   def destroy
     @item_comment = ItemComment.find(params[:id])
     @item_comment.destroy
-    redirect_to storages_path
+    redirect_to edit_item_path(@item_comment.item_id)
   end
-  
+
   private
-  
+
   def item_comment_params
     params.require(:item_comment).permit(:comment, :item_id, :member_id)
   end
-  
+
 end
