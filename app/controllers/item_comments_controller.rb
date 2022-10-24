6a9e6b966556dca6013c035c50bce1ca3a3e3db1
EC2 Default User <ec2-user@ip-172-31-38-10.ap-northeast-1.compute.internal>
2022-10-24 04:27:23 +0000

    コメント機能　非同期

--- app/controllers/item_comments_controller.rb
+++ app/controllers/item_comments_controller.rb
@@ -1,15 +1,18 @@
 class ItemCommentsController < ApplicationController
 
   def create
+    @item = Item.find(params[:item_id])
     @item_comment = ItemComment.new(item_comment_params)
+    @item_comment.item_id = @item.id
     @item_comment.save
-    redirect_to edit_item_path(@item_comment.item_id)
+    #redirect_to edit_item_path(@item_comment.item_id)
   end
 
   def destroy
+    @item = Item.find(params[:item_id])
     @item_comment = ItemComment.find(params[:id])
     @item_comment.destroy
-    redirect_to edit_item_path(@item_comment.item_id)
+    #redirect_to edit_item_path(@item_comment.item_id)
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,11 @@ Rails.application.routes.draw do
   get "search" => "searches#search"
   resources :storages, only: [:new, :edit, :show, :index, :create, :update, :destroy]
 
-  resources :items, only: [:index, :show, :new, :edit, :create, :destroy, :update]
+  resources :items, only: [:index, :show, :new, :edit, :create, :destroy, :update] do
+    resources :item_comments, only: [:create, :destroy]
+  end
 
   resources :members, only: [:create, :destroy]
 
-  resources :item_comments, only: [:create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
