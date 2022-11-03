a67d2fac90054d70d5449cfd19fe78fd9adc73d2
EC2 Default User <ec2-user@ip-172-31-38-10.ap-northeast-1.compute.internal>
2022-11-03 07:21:32 +0000

    コメント追加変更

--- app/models/member.rb
+++ app/models/member.rb
@@ -3,6 +3,6 @@ class Member < ApplicationRecord
   has_many :item_comments, dependent: :destroy
   belongs_to :user
 
-  validates :name, presence:true
+  validates :name, presence:true, length: {maximum: 20}
 
 end
