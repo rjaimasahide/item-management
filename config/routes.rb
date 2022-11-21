737567538a080796ab34f62c95c673486acd34e8
EC2 Default User <ec2-user@ip-172-31-38-10.ap-northeast-1.compute.internal>
2022-11-21 13:01:53 +0000

    コメント追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get "users" => "homes#top"
+  
+  #検索機能
   get "search" => "searches#search"
+  
+  #タブ部分
   resources :storages, only: [:new, :edit, :show, :create, :update, :destroy]
 
+  #アイテム部分
   resources :items, only: [:show, :new, :edit, :create, :destroy, :update] do
     resources :item_comments, only: [:create, :destroy]
     get 'shopping_list', on: :collection
@@ -12,6 +17,7 @@ Rails.application.routes.draw do
     post 'update_new', on: :collection
   end
 
+  #メンバー部分
   resources :members, only: [:create, :destroy]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
